4976e31d3f55d1d580d9a992525400ab58456cbc
apal5 <apal5@intuit.com>
2020-10-20 01:04:36 +0530

    test

--- src/main/java/com/example/datastructure/core/A01_searchInsertDelete_unsortedArr.java
+++ src/main/java/com/example/datastructure/core/A01_searchInsertDelete_unsortedArr.java
@@ -11,12 +11,24 @@ public class A01_searchInsertDelete_unsortedArr {
 	 * linearly search for an element in O(n) time
 	 * total time O(n)
 	 * 
-	 * --INSERT--
+	 * 
+	 * --INSERT AT LAST--
 	 * insert after last index in O(1) time
 	 * total time O(1)
 	 * 
-	 * --DELETE--
-	 * first search for the specific element in O(n) time
+	 * --INSERT AT FIRST--
+	 * traverse the array from end to start and right shift each element by one position in O(n) time
+	 * insert new element at first index in O(1) time
+	 * total time O(n)
+	 * 
+	 * --INSERT AT GIVEN INDEX--
+	 * traverse the array from end to the given index and right shift each element by one position in O(n) time
+	 * insert new element at the given index in O(1) time
+	 * total time O(n)
+	 * 
+	 * 
+	 * --DELETE A GIVEN ELEMENT--
+	 * first search the given element in O(n) time
 	 * then left-shift all elements to its right in O(n) time
 	 * total time O(n)
 	 * 
--- src/main/java/com/example/datastructure/core/A02_searchInsertDelete_sortedArr.java
+++ src/main/java/com/example/datastructure/core/A02_searchInsertDelete_sortedArr.java
@@ -7,19 +7,20 @@ package com.example.datastructure.core;
  */
 public class A02_searchInsertDelete_sortedArr {
 	/*
-	 * -SEARCH-
-	 * search using binary search in O(log n) time
+	 * --SEARCH--
+	 * find using binary search in O(log n) time
 	 * total time O(log n)
 	 * 
-	 * -INSERT-
-	 * find out the expected position of the NEW element in O(log n) time
+	 * --INSERT--
+	 * use binary search to find out the expected position of the NEW element in O(log n) time
 	 * then right-shift all elements of right side in O(n) time
 	 * then insert it at its desired place O(1) time
 	 * total time O(n)
 	 * 
-	 * -DELETE-
+	 * --DELETE--
 	 * search the element using binary search in O(log n) time
 	 * left-shift all elements to its right in O(n) time
 	 * total time O(n)
+	 * 
 	 */
 }
