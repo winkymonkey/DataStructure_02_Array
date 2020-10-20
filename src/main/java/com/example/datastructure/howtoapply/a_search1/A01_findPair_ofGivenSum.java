c400d6615338862fcf7b8460c4399153fef2447b
apal5 <apal5@intuit.com>
2020-10-21 01:19:25 +0530

    test

--- src/main/java/com/example/datastructure/howtoapply/a_search1/A01_findPair_ofGivenSum.java
+++ src/main/java/com/example/datastructure/howtoapply/a_search1/A01_findPair_ofGivenSum.java
@@ -2,9 +2,9 @@ package com.example.datastructure.howtoapply.a_search1;
 
 /**
  * *****************************************************************************
- * Given an array A[] and a number X, check for pair in A[] with sum as X
+ * Given an array A[] and a number X, check for pair in A[] with sum as SUM
  * *****************************************************************************
- * I/P: 1, 4, 45, 6, 10, -8
+ * I/P: 1, 4, 45, 6, 10, -8		SUM=16
  * O/P: YES		//10+6
  * 
  * *****************************************************************************
@@ -27,7 +27,7 @@ public class A01_findPair_ofGivenSum {
 	 * 	 else if(A[l]+A[r] < SUM)
 	 * 		l++;
 	 * }
-	 * TIME -- O(n*log n) + O(n)~~O(n*log n)
+	 * TIME -- O(n*log n) + O(n) ~ O(n*log n)
 	 * SPACE -- O(1)
 	 * 
 	 * 
@@ -36,11 +36,11 @@ public class A01_findPair_ofGivenSum {
 	 * ---USE HASHING---
 	 * -----------------
 	 * Take one HASHMAP
-	 * for(i=0 to n-1){
-	 * 	 if(HASHMAP.contains(X-A[i]))
-	 * 		//print "found"
+	 * for(i=0 to n-1) {
+	 * 	 if(HASHMAP.contains(SUM-A[i]))
+	 * 		//print "found" A[i] and SUM-A[i]
 	 * 	 else
-	 * 		HASHMAP.put(A[i], X-A[i])
+	 * 		HASHMAP.put(A[i], 999999)
 	 * }
 	 * TIME -- O(n)
 	 * SPACE -- O(n)
