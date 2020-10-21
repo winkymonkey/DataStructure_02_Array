b9ea4dbe5db3fc003ea14c5aff5ee3003eafeff0
apal5 <apal5@intuit.com>
2020-10-21 21:32:34 +0530

    test

--- src/main/java/com/example/datastructure/core/B02_InsertionSort.java
+++ src/main/java/com/example/datastructure/core/B02_InsertionSort.java
@@ -41,4 +41,42 @@ public class B02_InsertionSort {
 		}
 		System.out.println(Arrays.toString(A));
 	}
+	
+	
+	/**
+	 * --------------------------
+	 * 		TIME COMPLEXITY
+	 * --------------------------
+	 * We have seen that for input size of 'n', we do (n-1) comparisons and (n-1) movements for each element.
+	 * And the above comparison & movement logic are applied for all (n-1) elements of the array.
+	 * We can try to improve the complexity by changing the comparison and movement logic
+	 * 
+	 * |--------------------------------------------------------------------------|----------------------|
+	 * |                                for each element                          |  for (n-1) elements  |
+	 * |--------------------------------------------------------------------------|----------------------|
+	 * |                          |  COMPARISON  |   MOVEMENT   |      TOTAL      |        TOTAL         |
+	 * |--------------------------|--------------|--------------|-----------------|----------------------|
+	 * | REGULAR                  |     O(n)     |     O(n)     | = O(n)+O(n)     |  = (n-1) * O(n)      |
+	 * |                          |              |              | = O(n)          |  = O(n^2)            |
+	 * |--------------------------|--------------|--------------|-----------------|----------------------|
+	 * | COMPARISON               |   O(log n)   |     O(n)     | = O(log n)+O(n) |  = (n-1)*O(log n)    |
+	 * | (using binary search to  |              |              | = O(n)          |  = O(n^2)            |
+	 * | reduce comparison time)  |              |              |                 |                      |
+	 * |--------------------------|--------------|--------------|-----------------|----------------------|
+	 * | MOVEMENT                 |     O(n)     |     O(1)     | = O(n)+O(1)     |  = (n-1)*O(n)        |
+	 * | (using double linked list|              |              | = O(n)          |  = O(n^2)            |
+	 * | to reduce movement time  |              |              |                 |                      |
+	 * |--------------------------|--------------|--------------|-----------------|----------------------|
+	 * 
+	 * So we can see that whatever we do, we are unable to improve complexity of Insertion sort
+	 * 
+	 * 
+	 * --------------------------
+	 *      SPACE COMPLEXITY
+	 * --------------------------
+	 * Whatever be the input size, we only need one extra variable KEY to store the current element
+	 * Hence the Space complexity is O(1)
+	 * As it doesn't require any extra space, this type of sorting is also called In-Place Sorting
+	 * 
+	 */
 }
--- src/main/java/com/example/datastructure/howtoapply/a_search3/A01_findMedian_fromStreamOfIntegers.java
+++ src/main/java/com/example/datastructure/howtoapply/a_search3/A01_findMedian_fromStreamOfIntegers.java
@@ -4,14 +4,16 @@ package com.example.datastructure.howtoapply.a_search3;
  * *****************************************************************************
  * Median in a stream of integers (running integers)
  * *****************************************************************************
- * When the input size is odd, we take the middle element of sorted data 
- * If the input size is even, we pick average of middle two elements in sorted stream
- * *****************************************************************************
- * STREAM: 5, 15, 1, 3
- * 		After reading 1st element of stream - 5 -> median - 5
- * 		After reading 2nd element of stream - 5, 15 -> median - 10
- * 		After reading 3rd element of stream - 5, 15, 1 -> median - 5
- * 		After reading 4th element of stream - 5, 15, 1, 3 -> median - 4
+ * Median means ---
+ *   if the input size is odd --- Median = middle element of sorted stream
+ *   if the input size is even -- Median = average of two middle elements of sorted stream
+ * 
+ * Example:
+ * A Stream of 5, 15, 1, 3
+ * 		After reading 1st element of stream -- 5           -> median -- 5
+ * 		After reading 2nd element of stream -- 5, 15       -> median -- 10
+ * 		After reading 3rd element of stream -- 5, 15, 1    -> median -- 5
+ * 		After reading 4th element of stream -- 5, 15, 1, 3 -> median -- 4
  * 
  * *****************************************************************************
  */
@@ -21,8 +23,11 @@ public class A01_findMedian_fromStreamOfIntegers {
 	 * ---INSERTION SORT---
 	 * --------------------
 	 * If we can sort the data as it appears, we can easily locate median element.
-	 * Insertion Sort is one such online algorithm that sorts the data appeared so far.
+	 * Insertion Sort is one such algorithm that sorts the data appeared so far.
+	 *  -- i.e. after sorting i-th element, the first i elements of array are sorted.
 	 * 
+	 * TIME --- O(n^2)
+	 * SPACE -- O(1)
 	 * 
 	 * 
 	 * ---------------------------------
