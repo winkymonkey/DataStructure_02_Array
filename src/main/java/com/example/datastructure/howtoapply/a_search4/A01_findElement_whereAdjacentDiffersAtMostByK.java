db15964ab2061c23b8f89ebdffd02804a3b5ccd6
apal5 <apal5@intuit.com>
2020-10-22 03:45:44 +0530

    test

--- src/main/java/com/example/datastructure/howtoapply/a_search4/A01_findElement_whereAdjacentDiffersAtMostByK.java
+++ src/main/java/com/example/datastructure/howtoapply/a_search4/A01_findElement_whereAdjacentDiffersAtMostByK.java
@@ -4,15 +4,15 @@ package com.example.datastructure.howtoapply.a_search4;
  * *****************************************************************************
  * Searching in an array where adjacent differ by at most k
  * *****************************************************************************
- * It's an array of integer where each element has a difference of at-most 'K' with its neighbor.
+ * It's an array of integers where each element has a difference of at-most 'K' with its neighbor
  * Given a key ELEM, we need to find the index value of ELEM
  * If ELEM occurs multiple times, return the first occurrence of ELEM
  * *****************************************************************************
- * I/P: {4, 5, 6, 7, 6},	K=1,	X=6
- * O/P: 2					//X=6 occurs at index=2
+ * I/P: {4, 5, 6, 7, 6},				K=1,	ELEM=6
+ * O/P: 2								//ELEM=6 occurs at index=2
  * 
- * I/P: {40, 50, 40, 50, 60, 80, 60},	K=20,	X=80
- * O/P: 5								//X=80 occurs at index=5
+ * I/P: {40, 50, 40, 50, 60, 80, 60},	K=20,	ELEM=80
+ * O/P: 5								//ELEM=80 occurs at index=5
  * 
  * *****************************************************************************
  */
@@ -24,17 +24,17 @@ public class A01_findElement_whereAdjacentDiffersAtMostByK {
 	 * Instead of doing linear search,
 	 * we can utilize the property which says the adjacent elements can differ at most by K
 	 *  
-	 * i=0;
-	 * while(i<length){
-	 * 	 if(A[i]==ELEM){
-	 * 		//return
-	 * 	 }
-	 * 	 else {
-	 * 		//From the given property of array, we always know that X must be at-least 'DIFF/K' away.
-	 * 		//Hence, instead of searching one-by-one, we can jump 'DIFF/K'.
-	 * 		DIFF = ELEM-A[i];
-	 * 		i = i+(DIFF/K);
-	 * 	 }
+	 * i = 0;
+	 * while(i<length) {
+	 *    if( A[i]==ELEM ) {
+	 *       //return
+	 * 	  }
+	 * 	  else {
+	 *       //From the special property of this array, we can say that ELEM must be at-least 'DIFF/K' away.
+	 *       //Hence, instead of searching one-by-one, we can jump 'DIFF/K'.
+	 *       DIFF = ELEM-A[i];
+	 *       i = i+(DIFF/K);
+	 * 	  }
 	 * }
 	 * 
 	 */
--- src/main/java/com/example/datastructure/howtoapply/a_search4/A02_findMajority.java
+++ src/main/java/com/example/datastructure/howtoapply/a_search4/A02_findMajority.java
@@ -5,7 +5,7 @@ package com.example.datastructure.howtoapply.a_search4;
  * Majority Element
  * *****************************************************************************
  * An element that appears more than n/2 times in an array A[] of size n is called Majority Element.
- * Write a function which takes an array and prints the majority element(if exists), 
+ * Write a function which takes an array and prints the majority element(if exists)
  * *****************************************************************************
  * I/P: {3, 3, 4, 2, 4, 4, 2, 4, 4}
  * O/P: 4
@@ -22,44 +22,49 @@ public class A02_findMajority {
 	 * -----------------
 	 * Take one HASHMAP
 	 * for (i=0 to n-1) {
-	 * 	 if(HASHMAP.containsKey(A[i])) {
-	 * 		value = HASHMAP.get(A[i]);
-	 * 		HASHMAP.put(A[i], value++)
-	 * 	 }
-	 * 	 else {
-	 * 		HASHMAP.put(A[i], 0)
-	 * 	 }
+	 *    if(HASHMAP.containsKey(A[i])) {
+	 *       value = HASHMAP.get(A[i]);
+	 *       HASHMAP.put(A[i], value++)
+	 * 	  }
+	 * 	  else {
+	 *       HASHMAP.put(A[i], 0)
+	 * 	  }
 	 * }
-	 * TIME -- O(n)
+	 * 
+	 * TIME --- O(n)
 	 * SPACE -- O(n)
 	 * 
 	 * 
 	 * 
-	 * ---------------
-	 * ---USING BST---
-	 * ---------------
-	 * Each node will contain one extra variable (count) along with existing (key, left-child, right-child)
+	 * -----------------
+	 * ---USE SORTING---
+	 * -----------------
+	 * Sort the array in O(n * log n) time
+	 * Now traverse the array and keep track of each repeating element
+	 * 
+	 * TIME --- O(n * log n)
+	 * SPACE -- O(1)
+	 * 
+	 * 
+	 * 
+	 * ---------------------------------------------
+	 * ---USING BST OR SELF BALANCING BINARY TREE---
+	 * ---------------------------------------------
+	 * Each node will contain one extra field (count) along with existing fields -- key, left-child, right-child
 	 * for (i=0 to n-1) {
-	 * 	 if(A[i] exists in BST) {
-	 * 		currenntNode.count++
-	 * 	 }
-	 * 	 else {
-	 * 		//insert A[i] in proper place in tree
-	 * 		if(currentNode.count == n/2){
-	 * 			return;
-	 * 		}
-	 * 	 }
+	 * 	  if(A[i] exists in BST) {
+	 *       currenntNode.count++
+	 * 	  }
+	 * 	  else {
+	 *       //insert A[i] in proper place in the BST
+	 *       if(currentNode.count == n/2) {
+	 *          return;
+	 *       }
+	 * 	  }
 	 * }
-	 * TIME -- O(n^2)
-	 * SPACE -- O(n)
 	 * 
-	 * 
-	 * 
-	 * ------------------------------
-	 * --SELF BALANCING BINARY TREE--
-	 * ------------------------------
-	 * same as above
-	 * TIME -- O(n * log n)
+	 * TIME --- O(n^2) 			[if it's BST]
+	 * TIME --- O(n * log n)	[if it's Self Balancing Binary Tree]
 	 * SPACE -- O(n)
 	 * 
 	 * 
