ad76ee1375a72c23ef0147b0935a03a1fd399f98
apal5 <apal5@intuit.com>
2020-10-25 11:23:39 +0530

    test

--- src/main/java/com/example/datastructure/howtoapply/c_merge/A03_unionIntersection.java
+++ src/main/java/com/example/datastructure/howtoapply/c_merge/A03_unionIntersection.java
@@ -6,38 +6,45 @@ package com.example.datastructure.howtoapply.c_merge;
  * *****************************************************************************
  * I/P: A[]={1, 3, 4, 5, 7}
  * 		B[]={2, 3, 5, 6}
- * O/P: UNION--{1, 2, 3, 4, 5, 6, 7}
- * 		ITERSECTION--{3, 5}
+ * 
+ * O/P: UNION --------- {1, 2, 3, 4, 5, 6, 7}
+ * 		ITERSECTION --- {3, 5}
  * 
  * *****************************************************************************
  */
 public class A03_unionIntersection {
 	/*
-	 * i=0;		//to traverse A[]
-	 * j=0;		//to traverse B[]
-	 * k1=0;	//to traverse UNION[]
-	 * k2=0;	//to traverse INTER[]
-	 * UNION[]={}	//to store result of union
-	 * INTER[]={}	//to store result of intersection
+	 * i = 0;								//to traverse A[]
+	 * j = 0;								//to traverse B[]
+	 * k1 = 0;								//to traverse UNION[]
+	 * k2 = 0;								//to traverse INTER[]
+	 * UNION[] = new int[A.length+B.length]		//to store result of union
+	 * INTER[] = new int[A.length+B.length]		//to store result of intersection
 	 * 
-	 * while(){
-	 * 	if(A[i]==B[j]){
-	 * 	  UNION[k1]=A[i];
-	 * 	  k1++;
-	 * 	  INTER[k2]=A[i];
-	 * 	  k2++;
-	 * 		i++;
-	 * 		j++;
-	 * 	}
-	 *  else {
-	 *    UNION[k1]=A[i];
-	 *    k1++;
-	 *    UNION[k1]=B[j];
-	 *    k1++;
-	 * 		i++;
-	 * 		j++;
-	 *  }
+	 * while(i<A.length && j<B.length) {
+	 *    if(A[i] < B[j]) {
+	 *       UNION[k1++] = A[i++];
+	 *    }
+	 *    else if(A[i] > B[j]) {
+	 *       UNION[k1++] = B[j++];
+	 *    }
+	 *    else {
+	 *       UNION[k1++] = A[i];
+	 *       INTER[k2++] = A[i];
+	 *       i++;
+	 *       j++
+	 *    }
 	 * }
+	 * 
+	 * while(i < A.length) {
+	 *    UNION[k1++] = A[i++];
+	 * }
+	 * while(j < B.length) {
+	 *    UNION[k1++] = A[j++];
+	 * }
+	 * 
+	 * TIME --- O(m+n)
+	 * SPACE -- O(m+n)
 	 * 
 	 */
 }
--- src/main/java/com/example/datastructure/howtoapply/c_merge/A04_commonInThreeSubArray.java
+++ src/main/java/com/example/datastructure/howtoapply/c_merge/A04_commonInThreeSubArray.java
@@ -4,9 +4,9 @@ package com.example.datastructure.howtoapply.c_merge;
  * *****************************************************************************
  * Find common elements in three sorted arrays
  * *****************************************************************************
- * I/P: A[]={1, 5, 10, 20, 40, 80}
- * 		B[]={6, 7, 20, 80, 100}
- * 		C[]={3, 4, 15, 20, 30, 70, 80, 120}
+ * I/P: A[] = {1, 5, 10, 20, 40, 80}
+ * 		B[] = {6, 7, 20, 80, 100}
+ * 		C[] = {3, 4, 15, 20, 30, 70, 80, 120}
  * O/P: 20, 80
  * 
  * *****************************************************************************
@@ -16,12 +16,11 @@ public class A04_commonInThreeSubArray {
 	 * --------------------
 	 * ---NAIVE APPROACH---
 	 * --------------------
-	 * three arrays A[], B[], C[]
 	 * find intersection of A[], B[]			//it's called IN1[]
 	 * now find intersection of IN1[], C[]		//it's called IN2[]
 	 * IN2[] is the answer
 	 * 
-	 * TIME -- O(n1+n2+n3)
+	 * TIME --- O(n1+n2+n3)
 	 * SPACE -- O(n1+n2+n3)
 	 * 
 	 * 
@@ -44,7 +43,7 @@ public class A04_commonInThreeSubArray {
 	 * 	 }
 	 * }
 	 * 
-	 * TIME -- O(n1+n2+n3)
+	 * TIME --- O(n1+n2+n3)
 	 * SPACE -- O(1)
 	 * 
 	 */
