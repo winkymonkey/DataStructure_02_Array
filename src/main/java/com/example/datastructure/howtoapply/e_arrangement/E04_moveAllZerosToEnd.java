a2bee18bee7cb5d9ae43a48df1c57c9a1fe956f9
apal5 <apal5@intuit.com>
2020-10-26 14:18:35 +0530

    test

--- src/main/java/com/example/datastructure/howtoapply/e_arrangement/E04_moveAllZerosToEnd.java
+++ src/main/java/com/example/datastructure/howtoapply/e_arrangement/E04_moveAllZerosToEnd.java
@@ -1,5 +1,34 @@
 package com.example.datastructure.howtoapply.e_arrangement;
 
-public class E04_moveAllZerosToEnd {
+/**
+ * *******************************************************************************************************
+ * Move all zeroes to end of array
+ * *******************************************************************************************************
+ * Given an array of random numbers, Push all the zero’s of a given array to the end of the array
+ * 
+ * *******************************************************************************************************
+ * I/P: {1, 2, 0, 4, 3, 0, 5, 0}
+ * O/P; {1, 2, 4, 3, 5, 0, 0, 0}
+ * 
+ * I/P: {1, 2, 0, 0, 0, 3, 6}
+ * O/P: {1, 2, 3, 6, 0, 0, 0}
+ * 
+ * *******************************************************************************************************
+ */
 
+public class E04_moveAllZerosToEnd {
+	/*
+	 * count = 0
+	 * for(int i=0 to n) {
+	 *    if(A[i] != 0) {
+	 *       A[count] = A[i]
+	 *       count++
+	 *    }
+	 * }
+	 * while(count < n) {
+	 *    A[count] = 0
+	 *    count++
+	 * }
+	 * 
+	 */
 }
